c782ceffb3e636cfae62adf0f399d9df4561ae54
Dean DeHart <DeHartDean@gmail.com>
2023-01-12 20:50:12 -0500

    .

--- secondary/vite.config.js
+++ secondary/vite.config.js
@@ -5,11 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
     host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 80, // you can replace this port with any port
+    port: 5173, // you can replace this port with any port
   },
 });
